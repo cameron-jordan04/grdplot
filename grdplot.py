6ad9c993f35cabbfa361ca66464e84f6dd407f2c
cameron-jordan04 <4cameronjordan@gmail.com>
2022-05-31 04:34:51 -0700

    began implementation of color_mode options 1- 5

--- grdplot.py
+++ grdplot.py
@@ -12,28 +12,20 @@ Purpose:
 USAGE INFORMATION:
  	REQUIRED:
 		--input='filename.grd' to specify the input file
-OPTIONAL:
+	OPTIONAL:
  	 	--output='filename.png' to specify the name of the output file
 	 	--perspective='[azimuth, elevation]' to specify the perspective
 		--color_style='color style'
 		--intensity_file='intensity_file.grd'
 		--color_mode='#' (enter a number 1-7)
+		--hist_equalization='[True, color/shade, linear/normal]'
 
  Functionality to Impliment
  -C [contour_control] (doesn't work with surftype='i')
 
  -G color_mode ** (creates various color maps based on different variables)
 
-        color_mode = 1: Color/gray fill.
-        color_mode = 2: Color/gray fill shaded by synthetic illumination.
-        color_mode = 3: Color/gray fill shaded by an intensity file.
-			             The -K option must be used to specify the intensity file.
-        color_mode = 4: Color/gray fill of slope magnitude.
-        color_mode = 5: Color/gray fill shaded by slope magnitude.
-        color_mode = 6: Image plot using embedded georeferencing.
-        color_mode = 7: Image plot using bounds specified with -R.
-
- -P pagesize
+ ?? -P pagesize
 
  -S [color/shade] (histogram equalization)
 
@@ -52,13 +44,14 @@ output_file = 'results/output.png'
 perspective_input = [-130, 30] #[azimuth, elevation]
 color_style = 'jet'
 intensity_file = ''
-color_mode=1
+color_mode='1'
+hist_equalization = [False, '', '']
 
 # CREATE LIST OF VARIABLES
-variables = [grid_input, output_file, perspective_input, color_style, intensity_file, color_mode]
+variables = [grid_input, output_file, perspective_input, color_style, intensity_file, color_mode, hist_equalization]
 
 # CREATE LIST OF SYS VARIABLE TAGS
-sysvariables = ['input', 'output', 'perspective', 'color_style', 'intensity_file', 'color_mode']
+sysvariables = ['input', 'output', 'perspective', 'color_style', 'intensity_file', 'color_mode', 'hist_equalization']
 
 # CHECK INPUTS
 if sys.argv[1] == "--help" or sys.argv[1] == "--h":
@@ -110,76 +103,350 @@ minlon, maxlon, minlat, maxlat, minz, maxz = r[0], r[1], r[2], r[3], r[4], r[5]
 
 ### -------------------- END - EXTRACT LOAD DATA --------------------
 
-pygmt.makecpt(
-	# REFERENCE TO cmap
-	cmap=variables[3],
-	#max, min, inc
-	series=f'{minz}/{maxz}/5',
-	# creates a uniform
-	continuous=True
-)
-
-# GMT figure to handle all plotting
-fig = pygmt.Figure()
-
 '''
-def set_grid():
-	if(color_mode == 1):
-		return variables[0]
-	elif(color_mode == 2): # not rendering differently than color_mode 1
-		illumination_grid = pygmt.grdgradient(grid=variables[0], radience=[270,30])
-		return illumination_grid
-	#elif(color_mode == 3):
-
 color_mode
 
 1 - standard
 
-2 - use shading method in .grdview and use grdgradient as .grd files
+2 - use shading method in .grdview and use grdgradient as .grd files (synthetic illumination)
 
-3 - use drapegrid parameter in .grdview and reference the intensity_file
+3 - use drapegrid parameter in .grdview and reference the intensity_file (shaded by intensity file)
 
 4 - use slope_file method of grdgradient (requires direction method to be specified), then pass the output
-(outgrid?) as the .grd in .grdview
+(outgrid?) as the .grd in .grdview (slope magnitude)
 
-5 - same as above but shade by slope magnitude
+5 - same as above but shade by slope magnitude (shaded by slope magnitude) [shading = ]
 '''
 
-fig.grdview(
-	# set grid
-    grid=variables[0],
-    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
-    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
-    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
-    perspective=variables[2],
-    # set labels to axis, and name the directional axis
-    frame=["xa", "ya", "WSNE"],
-    # M15c = 15 cm Mercator projection (map scale)
-    projection="M15c",
-    # sets relative z topo size to be 1.5 cm
-    zsize="1.5c",
-    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
-    surftype="s",
-    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
-    # contourpen="1p"
-)
+'''
+hist_equalization
 
-fig.colorbar(
-	# inherits the perspective of the render
-	perspective=True,
-	# sets frame for color bar
-	frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
-	# sets color of color bar
-	cmap=variables[3]
-)
+OPTION == [True, color, ]
 
-fig.savefig(
-	#accepts .eps file type (rotation is incorrect)
-	variables[1],
-	# If True, will crop the figure canvas (page) to the plot area.
-	crop=True,
-	# If True, will open the figure in an external viewer.
-	show=True,
-	# Set raster resolution in dpi.
-	dpi=300
-)
+- use histogram equalized grd in .grdview
+
+OPTION == [True, shade, ]
+
+- use histogram equalized grd into grdgradient, then pass gradient grid into [shading = ]
+
+--------------------------------------------------------------------------------
+
+## Linear Distribution ## OPTION = [True, , linear]
+
+divisions = 16
+linear = pygmt.grdhisteq.equalize_grid(grid=variables[0], divisions=divisions)
+# linear_dist = pygmt.grd2xyz(grid=linear, output_type="pandas")["z"]
+# pygmt.grdhisteq.compute_bins(grid=grid, divisions=divisions)
+
+fig.grdview(grid=linear)
+
+## Normal Distribution ## OPTION = [True, , normal]
+
+normal = pygmt.grdhisteq.equalize_grid(grid=variables[0], gaussian=True)
+#normal_dist = pygmt.grd2xyz(grid=normal, output_type="pandas")["z"]
+
+fig.grdview(grid=normal)
+
+'''
+
+# COLOR_MODE 1 - STANDARD
+if variables[5] == '1':
+	pygmt.makecpt(
+		# REFERENCE TO cmap
+		cmap=variables[3],
+		#max, min, inc
+		series=f'{minz}/{maxz}/5',
+		# creates a uniform
+		continuous=True
+	)
+
+	# GMT figure to handle all plotting
+	fig = pygmt.Figure()
+
+	fig.grdview(
+		# set grid
+	    grid=variables[0],
+	    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
+	    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
+	    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
+	    perspective=variables[2],
+	    # set labels to axis, and name the directional axis
+	    frame=["xa", "ya", "WSNE"],
+	    # M15c = 15 cm Mercator projection (map scale)
+	    projection="M15c",
+	    # sets relative z topo size to be 1.5 cm
+	    zsize="1.5c",
+	    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
+	    surftype="i",
+	    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
+	    # contourpen="1p"
+	)
+
+	fig.colorbar(
+		# inherits the perspective of the render
+		perspective=True,
+		# sets frame for color bar
+		frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
+		# sets color of color bar
+		cmap=variables[3]
+	)
+
+	fig.savefig(
+		#accepts .eps file type (rotation is incorrect)
+		variables[1],
+		# If True, will crop the figure canvas (page) to the plot area.
+		crop=True,
+		# If True, will open the figure in an external viewer.
+		show=True,
+		# Set raster resolution in dpi.
+		dpi=300
+	)
+
+# COLOR_MODE 2 - SYNTHETIC ILLUMINATION
+# not working - not rendering grdview image
+if variables[5] == '2':
+	# Create new .grd for synthetic illumination
+	dgrid = pygmt.grdgradient(grid=variables[0], radiance=[270, 30])
+
+	print(
+		pygmt.grdinfo(
+			grid=dgrid,
+		)
+	)
+
+	pygmt.makecpt(
+		# REFERENCE TO cmap
+		cmap=variables[3],
+		#max, min, inc
+		series=f'{minz}/{maxz}/5',
+		# creates a uniform
+		continuous=True
+	)
+
+	# GMT figure to handle all plotting
+	fig = pygmt.Figure()
+
+	fig.grdview(
+		# set grid
+	    grid=dgrid,
+		# set drapegrid
+		#drapegrid=dgrid,
+	    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
+	    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
+	    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
+	    perspective=variables[2],
+	    # set labels to axis, and name the directional axis
+	    frame=["xa", "ya", "WSNE"],
+	    # M15c = 15 cm Mercator projection (map scale)
+	    projection="M15c",
+	    # sets relative z topo size to be 1.5 cm
+	    zsize="1.5c",
+	    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
+	    surftype="i",
+	    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
+	    # contourpen="1p"
+	)
+
+	fig.colorbar(
+		# inherits the perspective of the render
+		perspective=True,
+		# sets frame for color bar
+		frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
+		# sets color of color bar
+		cmap=variables[3]
+	)
+
+	fig.savefig(
+		#accepts .eps file type (rotation is incorrect)
+		variables[1],
+		# If True, will crop the figure canvas (page) to the plot area.
+		crop=True,
+		# If True, will open the figure in an external viewer.
+		show=True,
+		# Set raster resolution in dpi.
+		dpi=300
+	)
+
+# COLOR_MODE 3 - SHADE BY INTENSITY FILE
+if (variables[5] == '3' and intensity_file != ''):
+	pygmt.makecpt(
+		# REFERENCE TO cmap
+		cmap=variables[3],
+		#max, min, inc
+		series=f'{minz}/{maxz}/5',
+		# creates a uniform
+		continuous=True
+	)
+
+	# GMT figure to handle all plotting
+	fig = pygmt.Figure()
+
+	fig.grdview(
+		# set grid
+	    grid=variables[0],
+		# set grid for shading
+		shading=variables[4],
+	    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
+	    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
+	    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
+	    perspective=variables[2],
+	    # set labels to axis, and name the directional axis
+	    frame=["xa", "ya", "WSNE"],
+	    # M15c = 15 cm Mercator projection (map scale)
+	    projection="M15c",
+	    # sets relative z topo size to be 1.5 cm
+	    zsize="1.5c",
+	    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
+	    surftype="i",
+	    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
+	    # contourpen="1p"
+	)
+
+	fig.colorbar(
+		# inherits the perspective of the render
+		perspective=True,
+		# sets frame for color bar
+		frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
+		# sets color of color bar
+		cmap=variables[3]
+	)
+
+	fig.savefig(
+		#accepts .eps file type (rotation is incorrect)
+		variables[1],
+		# If True, will crop the figure canvas (page) to the plot area.
+		crop=True,
+		# If True, will open the figure in an external viewer.
+		show=True,
+		# Set raster resolution in dpi.
+		dpi=300
+	)
+
+# COLOR_MODE 4 - FILL BY SLOPE MAGNITUDE
+# failed 'grdgradient [ERROR]: No filename provided' - slope_file command not working
+if variables[5] == '4':
+	pygmt.makecpt(
+		# REFERENCE TO cmap
+		cmap=variables[3],
+		#max, min, inc
+		series=f'{minz}/{maxz}/5',
+		# creates a uniform
+		continuous=True
+	)
+
+	slope_mag_grd = ''
+
+	pygmt.grdgradient(
+		grid=variables[0],
+		direction='a',
+		slope_file=slope_mag_grd,
+		outgrid=None
+	)
+
+	# GMT figure to handle all plotting
+	fig = pygmt.Figure()
+
+	fig.grdview(
+		# set grid
+	    grid=slope_mag_grd,
+	    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
+	    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
+	    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
+	    perspective=variables[2],
+	    # set labels to axis, and name the directional axis
+	    frame=["xa", "ya", "WSNE"],
+	    # M15c = 15 cm Mercator projection (map scale)
+	    projection="M15c",
+	    # sets relative z topo size to be 1.5 cm
+	    zsize="1.5c",
+	    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
+	    surftype="i",
+	    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
+	    # contourpen="1p"
+	)
+
+	fig.colorbar(
+		# inherits the perspective of the render
+		perspective=True,
+		# sets frame for color bar
+		frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
+		# sets color of color bar
+		cmap=variables[3]
+	)
+
+	fig.savefig(
+		#accepts .eps file type (rotation is incorrect)
+		variables[1],
+		# If True, will crop the figure canvas (page) to the plot area.
+		crop=True,
+		# If True, will open the figure in an external viewer.
+		show=True,
+		# Set raster resolution in dpi.
+		dpi=300
+	)
+
+# COLOR_MODE 5 - SHADE BY SLOPE MAGNITUDE
+if variables[5] == '5':
+	pygmt.makecpt(
+		# REFERENCE TO cmap
+		cmap=variables[3],
+		#max, min, inc
+		series=f'{minz}/{maxz}/5',
+		# creates a uniform
+		continuous=True
+	)
+
+	# GMT figure to handle all plotting
+	fig = pygmt.Figure()
+
+	fig.grdview(
+		# set grid
+	    grid=variables[0],
+		# set shading grid
+		shading = slope_mag_grd,
+	    # minlon, maxlon, minlat, and maxlat set above, variable 5 and 6 indicate elevation range
+	    region=[minlon, maxlon, minlat, maxlat, minz, maxz],
+	    # first variable = degrees azimuth (-180 to 180), second variable = degrees elevation (0 - 90) - both of perspective
+	    perspective=variables[2],
+	    # set labels to axis, and name the directional axis
+	    frame=["xa", "ya", "WSNE"],
+	    # M15c = 15 cm Mercator projection (map scale)
+	    projection="M15c",
+	    # sets relative z topo size to be 1.5 cm
+	    zsize="1.5c",
+	    # defines topological surface type; i = image, sm = surface + m (for mesh) -- does not complete
+	    surftype="i",
+	    # Set the contour pen thickness to "1p" (only for surface (s) or mesh (default), surface types)
+	    # contourpen="1p"
+	)
+
+	fig.colorbar(
+		# inherits the perspective of the render
+		perspective=True,
+		# sets frame for color bar
+		frame=["a100", "x+l'Elevation in (m)'", "y+lm"],
+		# sets color of color bar
+		cmap=variables[3]
+	)
+
+	fig.savefig(
+		#accepts .eps file type (rotation is incorrect)
+		variables[1],
+		# If True, will crop the figure canvas (page) to the plot area.
+		crop=True,
+		# If True, will open the figure in an external viewer.
+		show=True,
+		# Set raster resolution in dpi.
+		dpi=300
+	)
+
+# HIST_EQUALIZATION = [TRUE, COLOR, LINEAR]
+
+
+# HIST_EQUALIZATION = [TRUE, COLOR, NORMAL]
+
+
+# HIST_EQUALIZATION = [TRUE, SHADE, LINEAR]
+
+
+# HIST_EQUALIZATION = [TRUE, SHADE, NORMAL]
